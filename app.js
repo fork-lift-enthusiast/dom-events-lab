e75e58b4a965d7e88ce5c6734a3e2e8450c3fc81
bonnie <owu.owu.666@gmail.com>
2025-03-07 16:13:03 -0500

    I'm finished and I'm so fucking cool

--- app.js
+++ app.js
@@ -1,9 +1,34 @@
 /*-------------------------------- Constants --------------------------------*/
+const calculator = document.querySelector("#calculator");
+const display = document.querySelector(".display");
 
 /*-------------------------------- Variables --------------------------------*/
-
+let equation = [];
 /*------------------------ Cached Element References ------------------------*/
 
 /*----------------------------- Event Listeners -----------------------------*/
+calculator.addEventListener("click", (event) => {
+  if (
+    ["*", "/", "-", "+"].includes(equation[equation.length - 1]) &&
+    event.target.classList.contains("operator")
+  ) {
+    return;
+  }
+
+  display.innerText += event.target.innerText;
+
+  equation.push(event.target.innerText);
+  if (event.target.innerText === "=") {
+    console.log(equation.join(""));
+    equation.pop();
+    display.innerText = `${eval(equation.join(""))}`;
+    equation = [];
+  }
+  if (event.target.innerText === "C") {
+    display.innerText = "";
+    equation = [];
+  }
+});
 
 /*-------------------------------- Functions --------------------------------*/
+
