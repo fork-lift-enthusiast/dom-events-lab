75d5159c138eeacde155241668cde6dc273f41ed
bonnie <owu.owu.666@gmail.com>
2025-03-07 16:39:03 -0500

    explanation for befudling code

--- app.js
+++ app.js
@@ -1,13 +1,11 @@
 /*-------------------------------- Constants --------------------------------*/
 const calculator = document.querySelector("#calculator");
 const display = document.querySelector(".display");
-
 /*-------------------------------- Variables --------------------------------*/
 let equation = [];
-/*------------------------ Cached Element References ------------------------*/
-
 /*----------------------------- Event Listeners -----------------------------*/
 calculator.addEventListener("click", (event) => {
+  // checks if the last item in the equation array, and the click event are operators and skips the rest of the code if they are
   if (
     ["*", "/", "-", "+"].includes(equation[equation.length - 1]) &&
     event.target.classList.contains("operator")
@@ -29,6 +27,3 @@ calculator.addEventListener("click", (event) => {
     equation = [];
   }
 });
-
-/*-------------------------------- Functions --------------------------------*/
-
